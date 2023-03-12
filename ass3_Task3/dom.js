8fdbca03bb9fad6469dbd656724acbc9e2b211cc
priyanka31july <priyankad4040@gmail.com>
2023-03-12 15:44:03 +0530

    queryselector

--- ass3_Task3/dom.js
+++ ass3_Task3/dom.js
@@ -54,6 +54,7 @@ li[4].style.backgroundColor='blue';
 var secondItem = document.querySelector('.list-group-item:nth-child(2)');
 secondItem.style.backgroundColor='green';
 
+
 var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
 thirdItem.style.visibility='hidden';
 
