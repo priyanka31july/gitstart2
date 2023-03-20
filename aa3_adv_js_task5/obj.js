9f1129f7c7bafcbff43f887aa47d9213d523e9f6
priyanka31july <priyankad4040@gmail.com>
2023-03-20 15:56:48 +0530

    obj

---
+++ aa3_adv_js_task5/obj.js
@@ -0,0 +1,10 @@
+const myObject = {
+    name: "Alice",
+    age: 30,
+    occupation: "Software Engineer"
+  };
+  
+  for (let key in myObject) {
+    console.log(`${key}: ${myObject[key]}`);
+  }
+  
