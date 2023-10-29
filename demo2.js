769f374b6d1e332e77e4c0271bf6284e9a025a3a
priyanka31july <priyankad4040@gmail.com>
2023-10-30 01:02:22 +0530

    new

--- demo2.js
+++ demo2.js
@@ -1,1 +1,21 @@
-console.log("this is git");
+function print(s)
+{
+    
+for(let i=0;i<=s.length;i++)
+{
+ s[i]=i+1;
+  
+}
+return s
+}
+function add()
+{
+let s=["wssss sssss fff","cccc ssss sxxxaa ddsdsd"]
+   
+
+for(let i in st)
+{
+console.log(st[i])
+}
+}
+add()
